c26f9d8c94f88422d88f73d855528faacb4b52b4
Qirui Wang <typhoeus@Qiruis-MacBook-Pro.local>
2024-05-21 11:54:27 -0700

    update

--- Linked List/141_Linkedin_List_Cycle.py
+++ Linked List/141_Linkedin_List_Cycle.py
@@ -10,15 +10,11 @@ class Solution(object):
         :type head: ListNode
         :rtype: bool
         """
-
         fast = head
         slow = head
-
         while fast and fast.next:
             fast = fast.next.next
             slow = slow.next
-
             if fast == slow:
                 return True
-        
         return None
